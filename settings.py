3257bc6afd06264c4b0cff59fb38de489c8dc681
K0RSY <pgeorg2010@gmail.com>
2025-08-02 20:02:16 +0200

    Added three lines map

--- settings.py
+++ settings.py
@@ -59,4 +59,4 @@ TRAM_LOOKUP = {
     "dr": ("assets/tram/tram_down_right.png", (1, -1))
 }
 
-MAP_ORDER = ["just_a_railway", "big_crossway", "prone_to_destruction", "switchers", "squares", "multiconcentration"]
+MAP_ORDER = ["just_a_railway", "big_crossway", "prone_to_destruction", "switchers", "squares", "multiconcentration", "three_lines"]
